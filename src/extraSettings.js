14e52bc7636f21c8aa2ac28a7b880de729e58f31
koyoonyoung777 <83170010+koyoonyoung777@users.noreply.github.com>
2021-06-13 20:12:33 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -9,17 +9,17 @@ let extraScore = 1;
 let wuDi = true;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 0;
+let firstFruit = 10;
 
 // 水果合成反转：false 改为 true
-let reverseLevelUp = false;
+let reverseLevelUp = true;
 
 // 指定生成的水果：默认值: 不开启反转 0-5 开启反转 6-11，修改对应数字即可控制随机生成的水果范围
 const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值（0-10）0 为葡萄，9 为半个西瓜
 const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
 let setFruits = {
   // 指定前几次生成的水果，可填入任意数量的数字，0 为葡萄，9 为半个西瓜
-  startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 0, 1, 2, 2, 3],
+  startFruits: reverseLevelUp ? [10, 9, 8, 7, 6, 6] : [0, 0, 1, 2, 2, 3],
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
@@ -34,8 +34,8 @@ let fruitSlowDown = 3;
 // 点击右上方图标更换水果：false 改为 true 即可
 let clickChangeFruit = false;
 
-// 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+
+
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '뚝딱뚝딱게임세상';
